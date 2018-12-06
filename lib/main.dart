0b9a70ca0047057c535218d3a8975f4a517347b6
giming <giming@hsin-yi.org.tw>
2018-12-06 18:09:45 +0800

    first

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {}, 
+                  child: Text('Add Product')
+                )
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Text('Food Paradise')
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
